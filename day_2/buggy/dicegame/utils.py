dcf3c39e1ed29e289afc65eb70336ea6bdada78a
felixnordgren11 <felix.edu.nordgren@gmail.com>
2026-03-17 18:19:49 +0100

    fixed the buggy code

--- day_2/buggy/dicegame/utils.py
+++ day_2/buggy/dicegame/utils.py
@@ -3,8 +3,8 @@ class UnnecessaryError(Exception):
 
 
 def i_just_throw_an_exception():
-    value = 1
-    def some_inner_function():
+    value = +1
+    def some_inner_function(value):
         value += 1
 
     some_value = "I don't know what you were expecting"
